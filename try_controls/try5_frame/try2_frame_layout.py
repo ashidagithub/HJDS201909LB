966d3caf33f84fbfab9d9493b5f41c682115b8c4
Mike. Shi <ashida2016@outlook.com>
2019-12-31 13:08:50 +0800

    updated

---
+++ try_controls/try5_frame/try2_frame_layout.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+
+# ------------------------(max to 80 columns)-----------------------------------
+# author by : （学员ID)
+# created:  2019.11
+
+# Description:
+#   初步学习 WinForm 编程 ( Frame )
+# ------------------------(max to 80 columns)-----------------------------------
+
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox
+
+# create root window
+top_win = tk.Tk()
+
+# naming root window
+top_win.title('Hello World Window')
+
+# resize root window
+win_size_pos = '800x600'
+#win_size_pos = '360x60'
+top_win.geometry(win_size_pos)
+
+#------------------------------
+# Step1: Create frame
+frame_root1 = tk.Frame(top_win, bg="#646464", width=760, height=200)
+# frame_root1.pack()
+frame_root1.place(x=20, y=100)
+
+# Step2: Appedn other controls
+lbl_test = tk.Label(frame_root1, text='text in frame')
+lbl_test.place(x=20, y=20)
+
+# Step3: Appedn other controls
+lbl_test2 = tk.Label(top_win, text='text in TOP WIN')
+lbl_test2.place(x=20, y=20)
+#------------------------------
+
+
+# show window and get into event loop
+top_win.mainloop()
